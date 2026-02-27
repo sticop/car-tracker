a5d3b39963f74d740d38f63714ac557cc38546a6
sticop <hamim.adghirni@live.com>
2026-02-27 11:40:31 +0000

    Fix PHP warning for null REQUEST_METHOD

--- web/api.php
+++ web/api.php
@@ -10,7 +10,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, X-API-Key');
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
     http_response_code(200);
     exit;
 }
